d07da995938e638a05cf0e7900f607d86fb61cb1
Purwandi <free6300@gmail.com>
2020-07-26 15:04:10 +0700

    Add helm-chart manifest
    Signed-off-by: Purwandi <free6300@gmail.com>

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-docker build -t docker.pkg.github.com/purwandi/istio/frontend:latest ./frontend
-docker build -t docker.pkg.github.com/purwandi/istio/product:latest ./product
-docker build -t docker.pkg.github.com/purwandi/istio/review:latest ./review
+docker build -t registry.gitlab.com/purwandi/istio/frontend:latest ./frontend
+docker build -t registry.gitlab.com/purwandi/istio/product:latest ./product
+docker build -t registry.gitlab.com/purwandi/istio/review:latest ./review
 
-docker push docker.pkg.github.com/purwandi/istio/frontend
-docker push docker.pkg.github.com/purwandi/istio/product
-docker push docker.pkg.github.com/purwandi/istio/review
+docker push registry.gitlab.com/purwandi/istio/frontend
+docker push registry.gitlab.com/purwandi/istio/product
+docker push registry.gitlab.com/purwandi/istio/review
